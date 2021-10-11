26f68fc8c45067e72f81172f39662b75463f323d
mihir20 <mihir20121997@gmail.com>
2021-10-12 01:59:45 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const executor = require('child_process');
+const Promise = require('bluebird');
+const lz4 = require('lz4');
+const fs = require('fs');
+
+
+app.listen(port, () => {
+    // mozila();
+    // chrome();
+  console.log(`app listening at http://localhost:${port}`)
+//   executor.exec(`strings /home/o_0/.config/google-chrome/Default/Sessions | grep -E '^https?://''`)
+})
